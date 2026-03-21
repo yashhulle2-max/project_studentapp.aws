193e8284bce80df593e88555e6e54bae144a8a43
yashhulle2-max <yashhulle2@gmail.com>
2026-03-21 12:29:08 +0530

    Database

---
+++ init.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS student_db;
+
+USE student_db;
+
+CREATE TABLE IF NOT EXISTS `students` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `course` varchar(255) DEFAULT NULL,
+  `student_class` varchar(255) DEFAULT NULL,
+  `percentage` double DEFAULT NULL,
+  `branch` varchar(255) DEFAULT NULL,
+  `mobile_number` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
